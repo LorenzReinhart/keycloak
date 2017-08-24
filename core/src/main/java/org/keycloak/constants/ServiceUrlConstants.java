19ebfb345349b0966faee8a62244fdf9c7ef61ba
Lorenz Reinhart <lorenz-reinhart@web.de>
2017-08-24 10:51:19 +0200

    Adapted OP endpoints to match Mitreid 	Changed AUTH_PATH, TOKEN_PATH, REALM_INFO_PATH, and JWKS_URL

--- core/src/main/java/org/keycloak/constants/ServiceUrlConstants.java
+++ core/src/main/java/org/keycloak/constants/ServiceUrlConstants.java
@@ -23,13 +23,13 @@ package org.keycloak.constants;
  */
 public interface ServiceUrlConstants {
 
-    public static final String AUTH_PATH = "/realms/{realm-name}/protocol/openid-connect/auth";
-    public static final String TOKEN_PATH = "/realms/{realm-name}/protocol/openid-connect/token";
+    public static final String AUTH_PATH = "/authorize";
+    public static final String TOKEN_PATH = "/token";
     public static final String TOKEN_SERVICE_LOGOUT_PATH = "/realms/{realm-name}/protocol/openid-connect/logout";
     public static final String ACCOUNT_SERVICE_PATH = "/realms/{realm-name}/account";
-    public static final String REALM_INFO_PATH = "/realms/{realm-name}";
+    public static final String REALM_INFO_PATH = "/jwk";
     public static final String CLIENTS_MANAGEMENT_REGISTER_NODE_PATH = "/realms/{realm-name}/clients-managements/register-node";
     public static final String CLIENTS_MANAGEMENT_UNREGISTER_NODE_PATH = "/realms/{realm-name}/clients-managements/unregister-node";
-    public static final String JWKS_URL = "/realms/{realm-name}/protocol/openid-connect/certs";
+    public static final String JWKS_URL = "/jwk";
 
 }
